75404b40608f0e90c6c789c0d23495a4172cabc8
claire p <cpuckett9@gmail.com>
2020-08-20 15:16:53 -0500

    dragleave stlying

--- assets/js/script.js
+++ assets/js/script.js
@@ -196,6 +196,7 @@ var dropZoneDragHandler = function (event) {
   var taskListEl = event.target.closest(".task-list")
   if (taskListEl) {
     event.preventDefault();
+    taskListEl.setAttribute("style","background: rgba(68, 233, 255, 0.7); border-style: dashed;"); 
   }
 };
 
@@ -215,8 +216,17 @@ else if (statusType === "tasks-in-progress") {
 else if (statusType === "tasks-completed") {
   statusSelectEl.selectedIndex = 2;
 }
+dropZoneEl.removeAttribute("style");
 dropZoneEl.appendChild(draggableElement);
 };
+
+var dragLeaveHandler = function(event) {
+  console.dir(event.target);
+  var taskListEl = event.target.closest(".task-list");
+if (taskListEl) {
+  taskListEl.removeAttribute("style");
+}
+}
 
 pageContentEl.addEventListener("click", taskButtonHandler)
 
@@ -227,3 +237,5 @@ pageContentEl.addEventListener("dragstart", dragStartHandler)
 pageContentEl.addEventListener("dragover", dropZoneDragHandler)
 
 pageContentEl.addEventListener("drop", dropTaskHandler)
+
+pageContentEl.addEventListener("dragleave", dragLeaveHandler)
